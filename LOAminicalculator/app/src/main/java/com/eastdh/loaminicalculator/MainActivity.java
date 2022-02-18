eaec28456d89deb1de0b97b7b187fcdc8633aa78
eastdh <eastdh@gmail.com>
2022-02-18 22:49:26 +0900

    make intent MAIN

--- LOAminicalculator/app/src/main/java/com/eastdh/loaminicalculator/MainActivity.java
+++ LOAminicalculator/app/src/main/java/com/eastdh/loaminicalculator/MainActivity.java
@@ -2,13 +2,29 @@ package com.eastdh.loaminicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button orehaButton = findViewById(R.id.orehaButton);
+    Button settingButton = findViewById(R.id.settingButton);
+    Button jewelButton = findViewById(R.id.jewelButton);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        orehaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), OrehaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
